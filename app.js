c8eed84f4e01f34c9af4b6a17cf2312518e71c73
Vishnu Darshan Sanku <vishnu@Vishnus-MacBook-Pro.local>
2020-09-24 17:53:33 +0530

    Added timer and modal

--- app.js
+++ app.js
@@ -1,3 +1,136 @@
+// Header
+
+// Components
+const header = document.createElement('header');
+document.body.append(header);
+
+const topNav = document.createElement('div');
+topNav.classList.add('topnav');
+
+header.append(topNav);
+
+const navTitle = document.createElement('div');
+navTitle.classList.add('title');
+
+topNav.append(navTitle);
+
+const mainText = document.createElement('h1');
+mainText.innerText = 'Tile Flip';
+mainText.style.color = 'White';
+
+navTitle.append(mainText);
+
+const navOptions = document.createElement('div');
+navOptions.classList.add('nav-options');
+
+topNav.append(navOptions);
+
+const link1 = document.createElement('a');
+link1.setAttribute('href', '#');
+link1.classList.add('active');
+link1.classList.add('instructions');
+link1.innerText = 'Instructions';
+link1.setAttribute('onclick', 'openModal()');
+link1.setAttribute('id', 'instruct');
+
+navOptions.append(link1);
+
+const link2 = document.createElement('a');
+link2.setAttribute('href', 'dom.html');
+link2.setAttribute('id', 'timer');
+link2.innerText = 'Timer';
+
+navOptions.append(link2);
+
+const link3 = document.createElement('a');
+link3.setAttribute('href', 'dom.html');
+link3.innerText = 'Score';
+
+navOptions.append(link3);
+
+// End Navbar
+
+// Modal
+const instructionsModal = document.createElement('div');
+instructionsModal.classList.add('modal');
+instructionsModal.setAttribute('id', 'myModal');
+
+document.body.append(instructionsModal);
+
+const modalContent = document.createElement('div');
+modalContent.classList.add('modal-content');
+
+instructionsModal.append(modalContent);
+
+const closeSpan = document.createElement('span');
+closeSpan.classList.add('close');
+closeSpan.innerText = '*';
+modalContent.style.color = 'white';
+
+modalContent.append(closeSpan);
+
+const instructionsPara = document.createElement('p');
+instructionsPara.innerHTML = '<p>Hello</p>';
+
+modalContent.append(instructionsPara);
+
+// modal logic
+const modal = document.getElementById('myModal');
+const instructBtn = document.getElementById('instructions');
+const span = document.getElementsByClassName('close')[0];
+
+function openModal() {
+  instructionsModal.style.display = 'block';
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+// Modal end
+
+// Timer Logic
+const cur  = new Date()
+console.log(cur.getMinutes(), cur.getSeconds())
+const TIME_LIMIT = 10 ;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+
+startTimer();
+function onTimesUp() {
+  clearInterval(timerInterval);
+  alert('Game Over');
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById('instruct').innerHTML = formatTime(
+      timeLeft
+    );
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+// Timer End
+
 const grid = document.createElement('div');
 grid.classList.add('grid');
 
@@ -40,24 +173,29 @@ function createBoard() {
 createBoard();
 
 let count = 0;
-let card1 = false;
-let card2 = false;
+var prevItem = '';
 document.querySelectorAll('.card').forEach((item) => {
   item.addEventListener('click', function () {
     count += 1;
+    console.log(count);
     if (count === 1) {
-      let prevItem = item;
+      prevItem = item;
+      flipCard(item);
     }
     if (count === 2) {
-      flipCard(item);
-      console.log(prevItem);
-      if (item.classList.contains('umbrella') && card1 === true) {
-        alert('Score Raised');
+      if (
+        item.classList.contains('umbrella') &&
+        prevItem.classList.contains('umbrella')
+      ) {
+        flipCard(item);
+        console.log('Score Raised');
+      } else {
+        flipCard(item);
+        console.log('Failed Game');
+        return;
       }
-    }
-    if (count === 3) {
+
       count = 0;
-      console.log('Step Complete');
     }
 
     // item.classList.toggle('is-flipped');
