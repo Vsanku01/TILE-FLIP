60a11010fc8253ec6c21ba96280e87f11f544f71
Vishnu Darshan Sanku <vishnu@Vishnus-MacBook-Pro.local>
2020-09-24 20:49:14 +0530

    refactoring logic

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ navOptions.append(link2);
 
 const link3 = document.createElement('a');
 link3.setAttribute('href', 'dom.html');
+link3.setAttribute('id', 'score');
 link3.innerText = 'Score';
 
 navOptions.append(link3);
@@ -71,6 +72,7 @@ modalContent.append(closeSpan);
 
 const instructionsPara = document.createElement('p');
 instructionsPara.innerHTML = '<p>Hello</p>';
+instructionsPara.style.color = 'black';
 
 modalContent.append(instructionsPara);
 
@@ -91,9 +93,9 @@ window.onclick = function (event) {
 // Modal end
 
 // Timer Logic
-const cur  = new Date()
-console.log(cur.getMinutes(), cur.getSeconds())
-const TIME_LIMIT = 10 ;
+const cur = new Date();
+console.log(cur.getMinutes(), cur.getSeconds());
+const TIME_LIMIT = 120;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
@@ -108,9 +110,7 @@ function startTimer() {
   timerInterval = setInterval(() => {
     timePassed = timePassed += 1;
     timeLeft = TIME_LIMIT - timePassed;
-    document.getElementById('instruct').innerHTML = formatTime(
-      timeLeft
-    );
+    document.getElementById('timer').innerHTML = formatTime(timeLeft);
 
     if (timeLeft === 0) {
       onTimesUp();
@@ -156,7 +156,7 @@ function createBoard() {
     grid.appendChild(square);
     squares.push(square);
 
-    // test
+    // cards
     const cardFaceFront = document.createElement('div');
     cardFaceFront.classList.add('card__face');
     cardFaceFront.classList.add('card__face--front');
@@ -174,44 +174,96 @@ createBoard();
 
 let count = 0;
 var prevItem = '';
+let initialScore = 0;
+let flipCount = 0;
+let unFlipCount = 0;
 document.querySelectorAll('.card').forEach((item) => {
   item.addEventListener('click', function () {
     count += 1;
-    console.log(count);
     if (count === 1) {
-      prevItem = item;
-      flipCard(item);
+      flipCard(item, count);
     }
     if (count === 2) {
-      if (
-        item.classList.contains('umbrella') &&
-        prevItem.classList.contains('umbrella')
-      ) {
-        flipCard(item);
-        console.log('Score Raised');
-      } else {
-        flipCard(item);
-        console.log('Failed Game');
-        return;
-      }
+      flipCard(item, count);
+      //   if (
+      //     item.classList.contains('umbrella') &&
+      //     prevItem.classList.contains('umbrella')
+      //   ) {
+      //     flipCard(item, count);
+      //     link3.innerText = `Score: ${initialScore + 10}`;
+      //     flipCount += 2;
+      //     console.log('Score Raised', flipCount);
+      //   } else {
+      //     flipCard(item, count);
+      //     unFlipCount += 1;
+      //     console.log('Failed Game', unFlipCount);
+      //   }
+      //   count = 0;
+    }
 
-      count = 0;
+    if (count === 3) {
+      alert('You can only raise two cards at a time');
+      return;
     }
 
     // item.classList.toggle('is-flipped');
   });
 });
 
-function cardSelect(item) {
-  flipCard(item);
-}
-
-function flipCard(item) {
-  item.classList.toggle('is-flipped');
-  if (item.classList.contains('umbrella')) {
+function flipCard(item, count) {
+  console.log('Master count', count);
+  // case - 1(bom)
+  if (count === 1 && item.classList.contains('danger')) {
+    console.log('entering case-1 case');
+    item.classList.toggle('is-flipped');
+    unFlipCount += 1;
+    setTimeout(() => {
+      item.classList.toggle('is-flipped');
+    }, 2000);
+    // print
+    console.log('Flip Count', flipCount);
+    console.log('Unflip Count', unFlipCount);
     return;
   }
-  setTimeout(() => {
+
+  //   store umbrella
+  if (count === 1 && item.classList.contains('umbrella')) {
     item.classList.toggle('is-flipped');
-  }, 2000);
+    prevItem = item;
+    // print
+    console.log('Flip Count', flipCount);
+    console.log('Unflip Count', unFlipCount);
+    return;
+  }
+
+  //   case - 3 (umb,bom)
+  if (count === 2 && item.classList.contains('danger')) {
+    console.log('entering case - 3');
+    unFlipCount += 2;
+    prevItem.classList.toggle('is-flipped');
+    item.classList.toggle('is-flipped');
+    setTimeout(() => {
+      item.classList.toggle('is-flipped');
+    }, 2000);
+    // print
+    console.log('Flip Count', flipCount);
+    console.log('Unflip Count', unFlipCount);
+    return;
+  }
+
+  //   case - 4(umb,umb)
+
+  if (
+    count === 2 &&
+    item.classList.contains('umbrella') &&
+    prevItem.classList.contains('umbrella')
+  ) {
+    console.log('entering case - 4');
+    item.classList.toggle('is-flipped');
+    flipCount += 2;
+    // print
+    console.log('Flip Count', flipCount);
+    console.log('Unflip Count', unFlipCount);
+    return;
+  }
 }
